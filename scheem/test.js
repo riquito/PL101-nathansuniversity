b761b10610ee5d33accd7f5ff2a9cc77afab8dfe
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-05-06 18:38:10 +0200

    Added scheem (simplified scheme language) PEG with tests

---
+++ scheem/test.js
@@ -0,0 +1,19 @@
+var PEG = require('pegjs');
+var assert = require('chai').assert;
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync(__dirname+'/scheem.peg', 'utf-8');
+// Show the PEG grammar file
+console.log(data);
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+
+// Do tests
+assert.deepEqual(parse(""), undefined,"parse empty string");
+assert.deepEqual(parse("atom"), "atom","parse atom");
+assert.deepEqual(parse("+"), "+", "parse +");
+assert.deepEqual(parse("(a b c)"),["a", "b", "c"],"parse (a b c)");
+assert.deepEqual(parse("(+ x 3)"), ["+", "x", "3"],"parse (+ x 3)");
+assert.deepEqual(parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]],
+    "parse (+ 1 (f x 3 y))");
