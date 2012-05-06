0e53ec1f43ac4c10c3eb3aafbe4306918b940bdc
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-05-06 18:44:08 +0200

    Spaces are allowed around parenthesis

--- scheem/test.js
+++ scheem/test.js
@@ -17,3 +17,6 @@ assert.deepEqual(parse("(a b c)"),["a", "b", "c"],"parse (a b c)");
 assert.deepEqual(parse("(+ x 3)"), ["+", "x", "3"],"parse (+ x 3)");
 assert.deepEqual(parse("(+ 1 (f x 3 y))"), ["+", "1", ["f", "x", "3", "y"]],
     "parse (+ 1 (f x 3 y))");
+
+assert.deepEqual(parse("( +  x 3  )"), ["+", "x", "3"],"parse ( +  x 3  )");
+assert.deepEqual(parse("  ( + x 3) "), ["+", "x", "3"],"parse ( + x 3) with spaces around parentheses");
