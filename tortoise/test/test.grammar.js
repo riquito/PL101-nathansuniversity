178ea8ad3d3ccca6fc10f1a9e2733d6d6a4bf2fc
Riccardo Attilio Galli <riccardo@sideralis.org>
2012-05-21 23:36:16 +0200

    First commit for the tortoise language: wrote a grammar to read numbers

---
+++ tortoise/test/test.grammar.js
@@ -0,0 +1,53 @@
+var PEG = require('pegjs');
+var assert = require('chai').assert;
+var fs = require('fs'); // for loading files
+
+// Read file contents
+var data = fs.readFileSync(__dirname+'/../tortoise.peg', 'utf-8');
+
+// Create my parser
+var parse = PEG.buildParser(data).parse;
+
+// Do tests
+
+suite('general',function(){
+    
+    test("parse empty string",function(){
+        assert.deepEqual(parse(""), undefined);
+    });
+    
+});
+
+suite('numbers',function(){
+    
+    test("0",function(){
+        assert.deepEqual(parse("0"), 0);
+    });
+    
+    test("natural",function(){
+        assert.deepEqual(parse("3"), 3);
+        
+        assert.deepEqual(parse("42"), 42);
+    });
+    
+    test("integer",function(){
+        assert.deepEqual(parse("+3"), 3);
+        assert.deepEqual(parse("+42"), 42);
+        assert.deepEqual(parse("-3"), -3);
+        assert.deepEqual(parse("-42"), -42);
+        assert.deepEqual(parse("+0"), 0);
+        assert.deepEqual(parse("-0"), -0);
+    });
+    
+    test("real",function(){
+        assert.deepEqual(parse(".1"), 0.1);
+        assert.deepEqual(parse(".12"), 0.12);
+        assert.deepEqual(parse("0.1"), 0.1);
+        assert.deepEqual(parse("0.12"), 0.12);
+        assert.deepEqual(parse("5.1"), 5.1);
+        assert.deepEqual(parse("123.456"), 123.456);
+        assert.deepEqual(parse("-1.2"), -1.2);
+        assert.deepEqual(parse("-123.456"), -123.456);
+    });
+
+});
